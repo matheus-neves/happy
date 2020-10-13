d0ca9504e86d8509948cc4ee82a3ffedb03b75a6
Matheus Neves <matheusneves1989@gmail.com>
2020-10-13 19:53:07 -0300

    feat: Added validation with Yup

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 import Orphanage from '../models/Orphanage';
 import orphanagesView from '../views/orphanages_view';
 
@@ -38,29 +39,47 @@ export default {
     } = request.body;
 
     const requestImages = request.files as Express.Multer.File[];
+
     const images = requestImages.map(image => ({
       path: image.filename,
     }));
 
-    try {
-      const orphanagesRepository = getRepository(Orphanage);
+    const orphanagesRepository = getRepository(Orphanage);
 
-      const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-        images,
-      });
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images,
+    };
 
-      await orphanagesRepository.save(orphanage);
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required(),
+        }),
+      ),
+    });
 
-      return response.status(201).json(orphanage);
-    } catch (error) {
-      return response.json({ error });
-    }
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
   },
 };

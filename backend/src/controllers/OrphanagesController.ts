6349eb4d38ea378acfda77a44e228cc4ab9b6482
Matheus Neves <matheusneves1989@gmail.com>
2020-10-13 18:43:09 -0300

    feat: Added images migration

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async index(request: Request, response: Response): Promise<Response> {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body;
+
+    try {
+      const orphanagesRepository = getRepository(Orphanage);
+
+      const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+      });
+
+      await orphanagesRepository.save(orphanage);
+
+      return response.status(201).json(orphanage);
+    } catch (error) {
+      return response.json({ error });
+    }
+  },
+};

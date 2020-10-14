23e7c4957bd1390f125362e49184526a297b1080
Matheus Neves <matheusneves1989@gmail.com>
2020-10-14 16:05:41 -0300

    feat(OrphanagesMap): Integrating with the api, listing the orphanages

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { FiArrowRight, FiPlus } from 'react-icons/fi';
@@ -8,50 +8,72 @@ import { Container } from './styles';
 import mapMarkerImg from '../../images/map-marker.svg';
 
 import mapIcon from '../../utils/mapIcon';
+import api from '../../services/api';
 
-const OrphanagesMap: React.FC = () => (
-  <Container>
-    <aside>
-      <header>
-        <img src={mapMarkerImg} alt="Happy" />
-        <h2>Escolha um orfanato no mapa</h2>
-        <p>Muitas crianças estão esperando a sua visita :)</p>
-      </header>
+interface IOrphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
-      <footer>
-        <strong>Pelotas</strong>
-        <span>Rio Grande do Sul</span>
-      </footer>
-    </aside>
+const OrphanagesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState<IOrphanage[]>([]);
 
-    <Map
-      center={[-27.2092052, -49.6401092]}
-      zoom={15}
-      style={{ width: '100%', height: '100%' }}
-    >
-      <TileLayer
-        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-      />
+  useEffect(() => {
+    (async () => {
+      const response = await api.get<IOrphanage[]>('orphanages');
 
-      <Marker icon={mapIcon} position={[-27.2092052, -49.6401092]}>
-        <Popup
-          closeButton={false}
-          minWidth={240}
-          max-maxWidth={240}
-          className="map-popup"
-        >
-          Lar das meninas
-          <Link to="/orphanages/1">
-            <FiArrowRight size={20} color="#fff" />
-          </Link>
-        </Popup>
-      </Marker>
-    </Map>
+      setOrphanages(response.data);
+    })();
+  }, []);
 
-    <Link to="/orphanages/create">
-      <FiPlus size={32} color="#fff" />
-    </Link>
-  </Container>
-);
+  return (
+    <Container>
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Pelotas</strong>
+          <span>Rio Grande do Sul</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-31.7652805, -52.3395189]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        {orphanages.map(({ id, name, latitude, longitude }) => (
+          <Marker key={id} icon={mapIcon} position={[latitude, longitude]}>
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              max-maxWidth={240}
+              className="map-popup"
+            >
+              {name}
+              <Link to={`/orphanages/${id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
+      </Map>
+
+      <Link to="/orphanages/create">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </Container>
+  );
+};
 
 export default OrphanagesMap;

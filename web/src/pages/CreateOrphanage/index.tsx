6303ab48527115541999ece749aa2ae67d637582
Matheus Neves <matheusneves1989@gmail.com>
2020-10-14 15:18:17 -0300

    feat: Creating the sidebar component

---
+++ web/src/pages/CreateOrphanage/index.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import { FiPlus } from 'react-icons/fi';
+import Sidebar from '../../components/Sidebar';
+
+import mapMarkerImg from '../../images/map-marker.svg';
+
+import { Container, Form, InputBlock } from './styles';
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60],
+});
+
+const CreateOrphanage: React.FC = () => {
+  return (
+    <Container>
+      <Sidebar />
+
+      <main>
+        <Form>
+          <fieldset>
+            <legend>Dados</legend>
+
+            <Map
+              center={[-27.2092052, -49.6401092]}
+              style={{ width: '100%', height: 280 }}
+              zoom={15}
+            >
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              />
+
+              <Marker
+                interactive={false}
+                icon={happyMapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
+            </Map>
+
+            <InputBlock>
+              <label htmlFor="name">Nome</label>
+              <input id="name" />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea id="name" maxLength={300} />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="images">Fotos</label>
+
+              <div className="uploaded-image" />
+
+              <button className="new-image" type="button">
+                <FiPlus size={24} color="#15b6d6" />
+              </button>
+            </InputBlock>
+          </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <InputBlock>
+              <label htmlFor="instructions">Instruções</label>
+              <textarea id="instructions" />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="opening_hours">Nome</label>
+              <input id="opening_hours" />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+              <div className="button-select">
+                <button type="button" className="active">
+                  Sim
+                </button>
+                <button type="button">Não</button>
+              </div>
+            </InputBlock>
+          </fieldset>
+
+          <button className="confirm-button" type="submit">
+            Confirmar
+          </button>
+        </Form>
+      </main>
+    </Container>
+  );
+};
+
+export default CreateOrphanage;
+
+// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
---
+++ web/src/pages/Orphanage/index.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { FaWhatsapp } from 'react-icons/fa';
+import { FiClock, FiInfo } from 'react-icons/fi';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+
+import Sidebar from '../../components/Sidebar';
+
+import mapMarkerImg from '../../images/map-marker.svg';
+
+import {
+  Container,
+  Details,
+  DetailsImages,
+  DetailsContent,
+  MapContainer,
+  OpenDetails,
+  OpenDetailsHour,
+  OpenDetailsWeekends,
+  ContactButton,
+} from './styles';
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60],
+});
+
+const Orphanage: React.FC = () => {
+  return (
+    <Container>
+      <Sidebar />
+
+      <main>
+        <Details>
+          <img
+            src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+            alt="Lar das meninas"
+          />
+
+          <DetailsImages>
+            <button className="active" type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </button>
+          </DetailsImages>
+
+          <DetailsContent>
+            <h1>Lar das meninas</h1>
+            <p>
+              Presta assistência a crianças de 06 a 15 anos que se encontre em
+              situação de risco e/ou vulnerabilidade social.
+            </p>
+
+            <MapContainer>
+              <Map
+                center={[-27.2092052, -49.6401092]}
+                zoom={16}
+                style={{ width: '100%', height: 280 }}
+                dragging={false}
+                touchZoom={false}
+                zoomControl={false}
+                scrollWheelZoom={false}
+                doubleClickZoom={false}
+              >
+                <TileLayer
+                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+                <Marker
+                  interactive={false}
+                  icon={happyMapIcon}
+                  position={[-27.2092052, -49.6401092]}
+                />
+              </Map>
+
+              <footer>
+                <a href="/">Ver rotas no Google Maps</a>
+              </footer>
+            </MapContainer>
+
+            <hr />
+
+            <h2>Instruções para visita</h2>
+            <p>
+              Venha como se sentir mais à vontade e traga muito amor para dar.
+            </p>
+
+            <OpenDetails>
+              <OpenDetailsHour>
+                <FiClock size={32} color="#15B6D6" />
+                Segunda à Sexta <br />
+                8h às 18h
+              </OpenDetailsHour>
+              <OpenDetailsWeekends>
+                <FiInfo size={32} color="#39CC83" />
+                Atendemos <br />
+                fim de semana
+              </OpenDetailsWeekends>
+            </OpenDetails>
+
+            <ContactButton type="button">
+              <FaWhatsapp size={20} color="#FFF" />
+              Entrar em contato
+            </ContactButton>
+          </DetailsContent>
+        </Details>
+      </main>
+    </Container>
+  );
+};
+
+export default Orphanage;

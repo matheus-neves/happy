46a388e920792d4445e2b2c06ca576e64dc413fb
Matheus Neves <matheusneves1989@gmail.com>
2020-10-16 18:55:02 -0300

    feat(routes): Adding custom header to routes

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -4,14 +4,33 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+
+import Header from './components/Header';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const Routes: React.FC = () => (
   <NavigationContainer>
-    <Navigator headerMode="none" screenOptions={{ headerShown: false }}>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="OrphanagesMap" component={OrphanagesMap} />
       <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      <Screen
+        name="OrphanageData"
+        component={OrphanageData}
+        options={{
+          header: () => <Header title="Bla Bla" />,
+        }}
+      />
+      <Screen
+        name="SelectMapPosition"
+        component={SelectMapPosition}
+        options={{
+          headerShown: true,
+          header: () => <Header title="Selecione no mapa" />,
+        }}
+      />
     </Navigator>
   </NavigationContainer>
 );
